4d89affa960f2212191570bc367b045b67867866
Xu <xu@Xus-MacBook-Pro.local>
2021-11-11 16:52:35 -0500

    First Push

---
+++ app/src/main/java/com/example/ece_5725/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ece_5725;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registered accelerometer listener");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i){
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent){
+        Log.d(TAG, "OnSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+    }
+}
