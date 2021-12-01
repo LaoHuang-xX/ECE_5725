945d9c96bf1604778cdfbfc16c0922ef6bb59b83
Xu <xu@dhcp-vl2041-17586.eduroam.cornell.edu>
2021-12-01 16:13:52 -0500

    Finally fixed connection!!!!!

--- Android/app/src/main/java/com/example/ece_5725/MainActivity.java
+++ Android/app/src/main/java/com/example/ece_5725/MainActivity.java
@@ -22,11 +22,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView infoip, msg;
     String x_axis;
     String y_axis;
+    String z_axis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Log.d(TAG, "onCreate: Initializing Sensor Services");
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -47,9 +49,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent){
-        //Log.d(TAG, "OnSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
-        x_axis = "X Position: " + sensorEvent.values[0];
-        y_axis = "Y Position: " + sensorEvent.values[1];
+        Log.d(TAG, "OnSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+        x_axis = String.valueOf(sensorEvent.values[0]);
+        y_axis = String.valueOf(sensorEvent.values[1]);
+        z_axis = String.valueOf(sensorEvent.values[2]);
     }
 
     @Override
@@ -58,3 +61,67 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         server.onDestroy();
     }
 }
+/*
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private WebView webview;
+
+    Server server ;
+    String videoURL = "http://10.148.12.247" +
+            ":8020/";
+    TextView msg ;
+
+    String x,y,z;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        sensorManager.registerListener(MainActivity.this,accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        server = new Server (this);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "X: " + event.values[0] + " |Y: " + event.values[1]);
+
+        x = "xValue: "+event.values[0];
+        y = "yValue: "+event.values[1];
+        z = "zValue: "+event.values[2];
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        server.onDestroy();
+    }
+
+}*/
